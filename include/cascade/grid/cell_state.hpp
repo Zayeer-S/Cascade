ec2324bd3c235a9bdd216430cf0edc70b4e32854
A-abd <65906361+A-abd@users.noreply.github.com>
2025-12-09 15:10:36 +0000

    refactor(grid): correct typo in enum

--- include/cascade/grid/cell_state.hpp
+++ include/cascade/grid/cell_state.hpp
@@ -18,7 +18,7 @@ namespace cascade
         Smouldering, ///< cell is burning with reduced intensity
         Burned,      ///< Cell has complemetly burnt out
         Wet,         ///< Cell has been treated with water (fire resistanct)
-        Supressed,   ///< Cell has been completely extinguished
+        Suppressed,   ///< Cell has been completely extinguished
     };
 
     /**
@@ -42,8 +42,8 @@ namespace cascade
             return "Burned";
         case CellState::Wet:
             return "Wet";
-        case CellState::Supressed:
-            return "Supressed";
+        case CellState::Suppressed:
+            return "Suppressed";
         default:
             return "Unknown";
         }
@@ -65,7 +65,7 @@ namespace cascade
             return 'X';
         case CellState::Wet:
             return '~';
-        case CellState::Supressed:
+        case CellState::Suppressed:
             return '-';
         default:
             return '?';
@@ -97,7 +97,7 @@ namespace cascade
      * @return true if cell resists ignition, false otherwise
      */
     inline bool isFireResistance(CellState state) {
-        return state == CellState::Wet || state == CellState::Supressed || state == CellState::Burned;
+        return state == CellState::Wet || state == CellState::Suppressed || state == CellState::Burned;
     }
 }
 
